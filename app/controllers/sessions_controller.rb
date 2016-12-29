15764bfbc289335c507c29aabf67352f657d303a
hydrogen <hydrogenlee@github.com>
2016-12-29 19:40:59 +0800

    验证码

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      if verify_rucaptcha?([:session])
+      #if verify_rucaptcha?([:session])
       log_in user
       params[:session][:remember_me] == '1' ? remember_user(user) : forget_user(user)
       flash= {:info => "欢迎回来: #{user.name} :)"}
-      else
-        flash= {:danger => '验证码错误'}
-        end
+      #else
+      #  flash= {:danger => '验证码错误'}
+      #  end
     else
       flash= {:danger => '账号或密码错误'}
     end
