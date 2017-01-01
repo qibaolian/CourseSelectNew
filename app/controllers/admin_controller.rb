e31d3b818c6ee5b782638671d95adfd8f14bcb7b
ruby on rails <wangjinbao161@mails.ucas.ac.cn>
2017-01-01 20:17:59 +0800

    new

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,20 @@
+class AdminController < ApplicationController
+  before_action :admin_logged_in
+  def default
+  end
+  private
+
+  # Confirms a admin logged-in user.
+  def admin_logged_in
+    unless admin_logged_in?
+      redirect_to root_url, flash: {danger: '请登陆'}
+    end
+  end
+
+  # Confirms a  logged-in user.
+  def logged_in
+    unless logged_in?
+      redirect_to root_url, flash: {danger: '请登陆'}
+    end
+  end
+end
