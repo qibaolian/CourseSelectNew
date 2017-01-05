405ff25771f5fbfdff501c9b6d885f8fb7f4a905
adalgb <mymailli@163.com>
2017-01-05 19:55:01 +0800

    index page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,18 @@
 class HomesController < ApplicationController
+  def index
+    #@articles = inform::articles.all
+    @announcement=Inform::Article.all
+  end
+  def index2
 
+  end
+  def index3
+    #@articles = inform::articles.all
+    @announcement1=Inform::Article.all
+    #find_by_title(params[:request])
+
+    #@user_request = UserRequest.find(params[:request])
+  end
 
 
 end
